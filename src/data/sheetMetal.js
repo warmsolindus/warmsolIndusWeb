f4fd4322fe8ece292e481616f22bf4b312116643
warmsolindus <gm@warmsolindus.com>
2025-10-24 08:06:46 +0530

    Create sheetMetal.js

---
+++ src/data/sheetMetal.js
@@ -0,0 +1,54 @@
+const sheetMetal = [
+  {
+    imgUrl: "/images/almWMB.jpg",
+    title: "Aluminium sheet with or without moisture barrier",
+    subTitle:
+      "Aluminium exhibits moderate strength and very good corrosion resistance which makes it an ideal metal to use for jacketing on mechanical insulation systems. ",
+    links: "/metal-jacketing-materials/aluminium-sheet",
+  },
+
+  {
+    imgUrl: "/images/stainless-steel-coil.jpg",
+    title: "Stainless steel sheet with or without moisture barrier",
+    subTitle:
+      " Stainless steel sheets are versatile and durable materials widely used in industrial, architectural, and decorative applications.",
+    links: "/metal-jacketing-materials/stainless-steel-sheet",
+  },
+  {
+    imgUrl: "/images/corrugatedSheet.png",
+    title: "Corrugated Sheet",
+    subTitle:
+      "Corrugated sheets are manufactured for enhanced strength, rigidity, and durability applications. Available in Aluminium and Stainless Steel varients",
+    links: "/metal-jacketing-materials/corrugated-sheet",
+  },
+  {
+    imgUrl: "/images/perforated-stainless-steel-sheets.jpg",
+    title: "Perforated sheet",
+    subTitle:
+      "Stainless steel Perforated sheets are durable and versatile materials with precision-cut holes. ",
+    links: "/metal-jacketing-materials/perforated-sheet",
+  },
+  {
+    imgUrl: "/images/welded-wire-mesh-SS.jpg",
+    title: "Welded Wire Mesh (Stainless Steel)",
+    subTitle:
+      "Stainless steel welded wire mesh is a highly versatile and durable material that serves a wide range of industrial and commercial applications.  ",
+    links: "/metal-jacketing-materials/welded-wire-mesh",
+  },
+  {
+    imgUrl: "/images/leadSheetR.jpg",
+    title: "Lead Sheet",
+    subTitle:
+      "Lead sheets are versatile and widely used products that are known for their durability, resistance to corrosion, and malleability. They are primarily composed of pure lead, which provides excellent radiation shielding, soundproofing, and waterproofing properties.",
+    links: "/metal-jacketing-materials/lead-sheet",
+  },
+  {
+    imgUrl: "/images/MLVS.jpg",
+    title: "Mass Loaded Vinyl Sheet",
+    subTitle:
+      "Mass loaded vinyl sheets are high-density, flexible materials designed for superior sound insulation. These versatile sheets effectively reduce noise transmission in various applications, from construction to automotive industries.",
+    links: "/metal-jacketing-materials/mass-loaded-vinyl-sheet",
+  },
+];
+
+export default metalJacketingMaterials;
