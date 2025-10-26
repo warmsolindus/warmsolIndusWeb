c6805a11cfd14bebe498da56020e18d1348b4acb
warmsolindus <gm@warmsolindus.com>
2025-10-26 23:00:53 +0530

    Update sheetMetal.js

--- src/data/sheetMetal.js
+++ src/data/sheetMetal.js
@@ -19,35 +19,35 @@ const sheetMetal = [
     imgUrl: "/images/shm3.jpg",
     title: " Heat Textured Silica Cloth",
     subTitle:
-      "Corrugated sheets are manufactured for enhanced strength, rigidity, and durability applications. Available in Aluminium and Stainless Steel varients",
+      " Heat-textured silica cloth insulation is a woven textile made from high-purity, texturized silica fibers, designed for extreme high-temperature applications, such as furnace curtains and cable insulation",
     links: "/sheet-metal/#",
   },
   {
     imgUrl: "/images/shm4.jpg",
     title: "Sewing Threads",
     subTitle:
-      "Stainless steel Perforated sheets are durable and versatile materials with precision-cut holes. ",
+      "Insulation sewing threads are high-performance yarns made from materials like silica, fiberglass, ceramic fibers, Aramid (Kevlar), and stainless steel-reinforced cotton that are designed to withstand high temperatures and maintain their strength and flexibility in extreme environments",
     links: "/sheet-metal/#",
   },
   {
     imgUrl: "/images/shm5.jpg",
     title: "Lacing Hooks, Anchors",
     subTitle:
-      "Stainless steel welded wire mesh is a highly versatile and durable material that serves a wide range of industrial and commercial applications.  ",
+      " Insulation anchors and hooks are fasteners and accessories for installing thermal or acoustic insulation, available in various types like self-stick, perforated base, and lacing anchors for permanent and removable applications",
     links: "/sheet-metal/#",
   },
   {
     imgUrl: "/images/shm6.jpg",
     title: "Quilting Pins",
     subTitle:
-      "Lead sheets are versatile and widely used products that are known for their durability, resistance to corrosion, and malleability. They are primarily composed of pure lead, which provides excellent radiation shielding, soundproofing, and waterproofing properties.",
+      " Quilting pin is a metal fastener, typically 12 or 14 gauge, made of mild steel or stainless steel, with a sharp point and a welded retaining head. It is used to secure and provide support for insulation blankets, especially removable ones, usually lock with self locking washer",
     links: "/sheet-metal/#",
   },
   {
     imgUrl: "/images/shm7.jpg",
     title: "Sheet Metal Hand Tools",
     subTitle:
-      "Mass loaded vinyl sheets are high-density, flexible materials designed for superior sound insulation. These versatile sheets effectively reduce noise transmission in various applications, from construction to automotive industries.",
+      " Using specialized tools is not a luxury—it's a necessity for an insulation system that performs to its specifications. The right tool ensures safety, efficiency, and, most importantly, the long-term integrity of the insulation, correct tools minimizes material waste, reduces installation time, and guarantees that the insulation system performs as designed",
     links: "/sheet-metal/#",
   },
 ];
