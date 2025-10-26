26fc7313784a2921b968fd1e21e60e5666ccadb0
warmsolindus <gm@warmsolindus.com>
2025-10-27 00:22:10 +0530

    Update insulationMaterials.js

--- src/data/insulationMaterials.js
+++ src/data/insulationMaterials.js
@@ -22,14 +22,14 @@ const insulationMaterials = [
   },
   {
     imgUrl: "/images/PUF.png",
-    title: "Polyurethane rigid foam (PUF)",
+    title: "Polyurethane Rigid Foam (PUF)",
     subTitle:
       "Polyurethane (PUF) rigid foam is produced by combining a polyol blend with an isocyanate by adding blowing agent to initiate foaming process. ",
     links: "/insulation-materials/polyurethane-foam-PUF",
   },
   {
     imgUrl: "/images/calciumSilicate.jpg",
-    title: "Calcium cilicate",
+    title: "Calcium Silicate",
     subTitle:
       "Calcium Silicate insulation board and Pipe Cover are made from tobermolite crystal reinforced with non-asbestos ﬁbres. ",
     links: "/insulation-materials/calcium-silicate",
@@ -40,6 +40,34 @@ const insulationMaterials = [
     subTitle:
       "Perlite Insulation is the waterproof heat insulation material that is asbestos-free and of high intensity with high quality perlite particles as the main material and made by cohering inorganic bond and reinforced fiber. ",
       links: "/insulation-materials/perlite",
+  },
+  {
+    imgUrl: "/images/aerogel.jpg",
+    title: "Aerogel Blankets",
+    subTitle:
+      "Aerogel insulation blanket is a thin, flexible, and highly effective thermal insulation material made by embedding silica aerogel into a durable fiber matrix, These blankets are also lightweight, fire-resistant, and can operate across a wide range of temperatures.",
+      links: "/insulation-materials/#",
+  },
+  {
+    imgUrl: "/images/phinolic.jpg",
+    title: " Phinolic Insulation",
+    subTitle:
+      " Phenolic insulation is a rigid, closed-cell foam made from phenolic resin, known for its exceptional thermal efficiency, making it one of the most effective common insulation materil. It features a high R value, low moisture absorption, and superior fire performance because it chars and self-extinguishes when exposed to flames.",
+      links: "/insulation-materials/#",
+  },
+  {
+    imgUrl: "/images/mineralfiber.jpg",
+    title: "Mineral Fiber Wool",
+    subTitle:
+      "Mineral fiber insulation functions by trapping air within its interlocking fibers, which slows the transfer of heat and sound, making it effective for thermal and acoustic insulation ",
+      links: "/insulation-materials/#",
+  },
+  {
+    imgUrl: "/images/elasto.jpg",
+    title: "Elastomeric Insualtion",
+    subTitle:
+      " Closed-cell elastomeric insulation is a flexible, rubber-like material primarily used to insulate mechanical systems like HVAC and refrigeration pipes and ductwork. Its non-porous, closed-cell structure provides excellent thermal resistance, moisture control, and vibration dampening",
+      links: "/insulation-materials/#",
   },
 ];
 
