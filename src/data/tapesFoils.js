5b530620c12314991a25a2fe527de9e032420f3f
warmsolindus <gm@warmsolindus.com>
2025-11-02 17:37:57 +0530

    Update tapesFoils.js
    udates pending

--- src/data/tapesFoils.js
+++ src/data/tapesFoils.js
@@ -1,1 +1,54 @@
+const tapesFoils = [
+  {
+    imgUrl: "/images/tfo1.jpg",
+    title: "Fiber Glass Tape",
+    subTitle:
+      " Fiberglass tape is made up of twisted fiberglass threads. These strands are braided at right angles to form a specific width of fiberglass cloth. In order to prevent the cloth from spreading, it contains the edge of the cloth edge",
+    links: "/tapes-foils/fiber-glass-tape",
+  },
 
+  {
+    imgUrl: "/images/tfo2.jpg",
+    title: "Filament Tape",
+    subTitle:
+      " Filament tape offers exceptional tensile strength, durability, and versatility for a wide range of packaging and industrial applications. Engineered with reinforcing fibers, this tape provides superior performance in securing heavy loads, bundling, and palletizing.",
+    links: "/tapes-foils/filament-tape",
+  },
+  {
+    imgUrl: "/images/tfo3.jpg",
+    title: " Re-Inforced Aluminum Foil Tape",
+    subTitle:
+      "Aluminum foil tape is suitable for smooth flat and irregular surfaces. It is generally used for sealing hot and cold air ducts (excellent HVAC duct tape), pipe insulation systems, sealing aluminum, stainless steel and plastic",
+    links: "/tapes-foils/re-inforced-aluminium",
+  },
+  {
+    imgUrl: "/images/tfo4.jpg",
+    title: "Sewing Threads",
+    subTitle:
+      "Insulation sewing threads are high-performance yarns made from materials like silica, fiberglass, ceramic fibers, Aramid (Kevlar), and stainless steel-reinforced cotton that are designed to withstand high temperatures and maintain their strength and flexibility in extreme environments",
+    links: "/tapes-foils/#",
+  },
+  {
+    imgUrl: "/images/tfo5.jpg",
+    title: "Lacing Hooks, Anchors",
+    subTitle:
+      " Insulation anchors and hooks are fasteners and accessories for installing thermal or acoustic insulation, available in various types like self-stick, perforated base, and lacing anchors for permanent and removable applications",
+    links: "/tapes-foils/#",
+  },
+  {
+    imgUrl: "/images/tfo6.jpg",
+    title: "Quilting Pins",
+    subTitle:
+      " Quilting pin is a metal fastener, typically 12 or 14 gauge, made of mild steel or stainless steel, with a sharp point and a welded retaining head. It is used to secure and provide support for insulation blankets, especially removable ones, usually lock with self locking washer",
+    links: "/tapes-foils/#",
+  },
+  {
+    imgUrl: "/images/tfo7.jpg",
+    title: "Sheet Metal Hand Tools",
+    subTitle:
+      " Using specialized tools is not a luxury—it's a necessity for an insulation system that performs to its specifications. The right tool ensures safety, efficiency, and, most importantly, the long-term integrity of the insulation, correct tools minimizes material waste, reduces installation time, and guarantees that the insulation system performs as designed",
+    links: "/tapes-foils/#",
+  },
+];
+
+export default sheetMetal;
